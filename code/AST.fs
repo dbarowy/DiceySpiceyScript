0b29f75fc82dd041947816b327af0a1d3b6f038c
Maia Wang <maiawang@contact.com>
2024-05-08 10:20:36 -0400

    Added recipe type

--- code/AST.fs
+++ code/AST.fs
@@ -4,3 +4,11 @@ type Expr =
 | Instruction of string
 | Ingredient of string
 | Title of string
+
+type Recipe = 
+    {
+        Title: Expr;
+        Ingredients: Expr list;
+        Instructions: Expr list; 
+    }
+
