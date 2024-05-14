387b316cf408aa0d584c6882ecfdf5362f00810a
Helen Qian <helenqian@Helens-MacBook-Air.local>
2024-05-13 21:01:23 -0400

    testing

--- code/tests/Tests.fs
+++ code/tests/Tests.fs
@@ -1,17 +1,31 @@
 namespace RecipeTests
 open System
 open Microsoft.VisualStudio.TestTools.UnitTesting
-open code
+open Parser
+open Evaluator
+open AST
 
 [<TestClass>]
 type TestClass () =
     [<TestMethod>]
     member this.ingredient () =
-        let input = "ing[lettuce, corn, beef, dorito chips]"
-        let expected = [ "lettuce"; "corn"; "beef"; "dorito chips"]
-        let result = parse (prepare input)
+        let input = "ing[lettuce, corn, beef, chips]"
+        let expected = [ [Instruction("lettuce")] ; [Instruction("corn")] ; [Instruction("beef")] ; [Instruction("chips")]]
+        let result = parse input
         match result with
         | Some ws ->
             Assert.AreEqual(expected, ws)
         | None ->
             Assert.IsTrue false
+
+    // [<TestMethod>]
+    //     member this.evalIngredient () =
+    //         let input = "ing[lettuce, corn, beef, chips]"
+    //         let expected = [ @"\item lettuce \n"; @"\item corn \n"; @"\item beef \n"; @"\item chips \n"]
+    //         let result = parse (input)
+    //         match result with
+    //         | Some ws ->
+    //             Assert.AreEqual(expected, ws)
+    //         | None ->
+    //             Assert.IsTrue false
+
