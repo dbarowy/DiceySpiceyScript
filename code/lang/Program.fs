bba14883a6c32b2e6dd14d54549f2aa63236de38
Helen Qian <helenqian@Helens-MacBook-Air.local>
2024-05-16 17:26:10 -0400

    working on getting pdflatex to work

--- code/lang/Program.fs
+++ code/lang/Program.fs
@@ -1,9 +1,56 @@
 ﻿open Parser
 open Evaluator
 open System.IO
+open System.Diagnostics
+open System.Threading.Tasks
+
+// type CommandResult = 
+//         {
+//             ExitCode: int;
+//             StandardOutput: string;
+//             StandardError: string
+//         }
+
+// let executeCommand executable args : CommandResult =
+//         async {
+//             let! ct = Async.CancellationToken
+
+//             let startInfo = ProcessStartInfo()
+//             startInfo.FileName <- executable
+//             startInfo.RedirectStandardOutput <- true
+//             startInfo.RedirectStandardError <- true
+//             startInfo.UseShellExecute <- false
+//             startInfo.CreateNoWindow <- true
+//             for a in args do
+//             startInfo.ArgumentList.Add(a)
+
+//             use p = new Process()
+//             p.StartInfo <- startInfo
+//             p.Start() |> ignore
+
+//             let outTask =
+//                 Task.WhenAll([|
+//                     p.StandardOutput.ReadToEndAsync(ct);
+//                     p.StandardError.ReadToEndAsync(ct) |])
+
+//             do! p.WaitForExitAsync(ct) |> Async.AwaitTask
+//             let! out = outTask |> Async.AwaitTask
+
+//             return 
+//               { ExitCode = p.ExitCode
+//                 StandardOutput = out.[0]
+//                 StandardError = out.[1] 
+//               }
+//         }
+
+
+// let executeShellCommand command =
+//     executeCommand "/usr/bin/env" [ "-S"; "bash"; "-c"; command ]
+
 
 [<EntryPoint>]
 let main argv : int =
+
     (* Check for proper usage *)
     if argv.Length <> 1 && argv.Length <> 2 then
         printfn "Usage: dotnet run <file> \n file contains format: \n \ttit[title of recipe]
@@ -24,7 +71,14 @@ let main argv : int =
     (* try to evaluate what we parsed... or not *)
     match ast_maybe with
     | Some ast ->
-        printf "%A" (eval ast)
+        let s= (eval ast)
+        File.WriteAllText("temp2.tex",s)
+        //let r = executeShellCommand "pdflatex"
+        // if r.ExitCode = 0 then
+        //    //printfn "%s" r.StandardOutput
+        // else
+        //     eprintfn "%A" "error"
+        //     exit 1
         0
     | None ->
         printfn ("file contains invalid program: file should contain format: \n \ttit[title of recipe]
--- code/tests/Tests.fs
+++ code/tests/Tests.fs
@@ -18,13 +18,49 @@ type TestClass () =
             Assert.AreEqual(expected, ws)
         | None ->
             Assert.IsTrue false
+    
+    [<TestMethod>]
+    member this.instruction () =
+        let input = "ins[Grease pan, mix in all ingredients for batter, pour batter into round cake pan, bake for 25 min]"
+        let expected = [[Instruction("Grease pan") ; Instruction("mix in all ingredients for batter") ; Instruction("pour batter into round cake pan") ; Instruction("bake for 25 min")]]
+        let result = parse ( input)
+        match result with
+        | Some ws ->
+            Assert.AreEqual(expected, ws)
+        | None ->
+            Assert.IsTrue false
+    
+    [<TestMethod>]
+    member this.title () =
+        let input = "tit[Apple Pie]"
+        let expected = [[Title("Apple Pie")]]
+        let result = parse ( input)
+        match result with
+        | Some ws ->
+            Assert.AreEqual(expected, ws)
+        | None ->
+            Assert.IsTrue false
 
             
     [<TestMethod>]
-        member this.evalIngredient () =
+        member this.evalTitle () =
             let input = Title("cake")
             let expected = @"\title{\Huge\fontfamily{lmdh}\selectfont" + "\n" + "cake" + "\n}" + "\n" + @"\date{}" + "\n" + @"\maketitle" + "\n" + "\n"
             let result2 = evalTitle (input)
             Assert.AreEqual(expected, result2)
+
+    [<TestMethod>]
+        member this.evalInstruction () =
+            let input = [Instruction("Grease Pan")] @ [Instruction("Dance")]
+            let expected =  @"\item " + "Grease Pan" + "\n" + @"\item " + "Dance" + "\n"
+            let result2 = evalInstruction (input)
+            Assert.AreEqual(expected, result2)
             
+
+    [<TestMethod>]
+        member this.evalIngredient () =
+            let input = [Ingredient("Orange")] @ [Ingredient("A voc a fresh a do")]
+            let expected = @"\item " + "Orange"  + "\n" + @"\item " + "A voc a fresh a do"  + "\n"
+            let result2 = evalIngredient (input)
+            Assert.AreEqual(expected, result2)
 
